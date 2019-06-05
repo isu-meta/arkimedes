9e2ee31e8ad3e3262a0cf36e6ea9ef3104ac10da
wtee <wtee@users.noreply.github.com>
2019-06-05 10:00:02 -0500

    Remove print statements

--- arkimedes.py
+++ arkimedes.py
@@ -4,7 +4,7 @@ from arkimedes import get_xml_from_urls
 from arkimedes.ead import generate_anvl_fields_from_ead_xml
 from arkimedes.ezid import upload_anvl
 
-# arkimedes un pass ark:/sjksk outfile ead --anvil-file 1 2 3 ...
+# arkimedes un pass ark:/sjksk outfile.txt ead --anvil-file anvl.txt 1 2 3 ...
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,11 +19,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print(args)
-
     if args.type == "ead":
         ead_xml = get_xml_from_urls(args.sources)
-        # print(ead_xml)
 
         if args.anvl_file:
             anvls = generate_anvl_fields_from_ead_xml(ead_xml, args.anvl_file)
@@ -31,7 +28,6 @@ if __name__ == "__main__":
             anvls = generate_anvl_fields_from_ead_xml(ead_xml)
 
         for anvl in anvls:
-            print(anvl)
             upload_anvl(
                 args.username, args.password, args.shoulder, anvl, args.output_file
             )
