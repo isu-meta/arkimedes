d614a402bcde8263a2cf23dbcebccf191d3ce435
wtee <wtee@users.noreply.github.com>
2019-06-30 11:12:59 -0500

    Rewrite db.py; add config.py
    Rewrite db.py to use sqlalchemy. Add config.py to manage db related
    variables. Add load_arks_from_ezid_anvl to ezid.py and reorder
    function definitions.

--- arkimedes/db.py
+++ arkimedes/db.py
@@ -1,66 +1,126 @@
-from collections import namedtuple
-import sqlite3
+from pathlib import Path
+import re
 
-ARKRecord = namedtuple(
-    "ARKRecord",
-    (
-        "ark",
-        "profile",
-        "dc_publisher",
-        "target",
-        "erc_when",
-        "ownergroup",
-        "status",
-        "erc_what",
-        "erc_who",
-        "export",
-        "dc_title",
-        "updated",
-        "dc_type",
-        "dc_date",
-        "owner",
-        "dc_creator",
-        "created",
-    ),
-)
+from sqlalchemy import Column, create_engine, Integer, String, Text, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-ARKRecord.__doc__ = """A friendly representation of an ARK record.
-                    The seemingly-arbitrary attribute order reflects the
-                    order used in EZID ANVL and XML records."""
+from arkimedes.config import DB_CONN, DB_TYPE, SQLITE_FILE
+from arkimedes.ezid import load_arks_from_ezid_anvl
+
+engine = create_engine(DB_CONN)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 
-class DB:
-    """A thin wrapper for SQLite access."""
+class Ark(Base):
+    __tablename__ = "arks"
 
-    def __init__(self, addr):
-        self.address = addr
-        self.connection = None
-        self.cursor = None
+    ark = Column(String(30), primary_key=True)
+    target = Column(Text)
+    profile = Column(String(8))
+    status = Column(String(11))
+    owner = Column(String(11))
+    ownergroup = Column(String(11))
+    created = Column(Integer)
+    updated = Column(Integer)
+    export = Column(Boolean)
+    dc_creator = Column(Text)
+    dc_title = Column(Text)
+    dc_type = Column(String(20))
+    dc_date = Column(Text)
+    dc_publisher = Column(Text)
+    erc_when = Column(Text)
+    erc_what = Column(Text)
+    erc_who = Column(Text)
+    replaceable = Column(Boolean)
 
-    def open_db(self):
-        self.connection = sqlite3.connect(self.address)
-        self.cursor = self.connection.cursor()
 
-    def close_db(self):
-        self.connection.close()
-        self.connection = None
-        self.cursor = None
+def create_db(engine):
+    Base.metadata.create_all(engine)
 
-    def _execute_edit(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
 
-    def create_table(self, name, fields):
-        self._execute_edit(f"CREATE TABLE {name} {fields}")
+def db_exists():
+    global DB_TYPE
+    global SQLITE_FILE
 
-    def insert_into_table(self, table, values):
-        self._execute_edit(f"INSERT INTO {table} {values}")
+    if DB_TYPE == "sqlite":
+        return Path(SQLITE_FILE).exists()
 
-    def update_column(self, table, field, value, condition):
-        self._execute_edit(f"UPDATE {table} SET {field} = {value} WHERE {condition}")
 
-    def delete_row(self, table, condition):
-        self._execute_edit(f"DELETE FROM {table} WHERE {condition}")
+def load_batch_download_file_into_db(batch_file):
+    arks = load_arks_from_ezid_anvl(batch_file)
+    session = Session()
 
-    def find(self, table, field, condition):
-        self.cursor.execute(f"SELECT FROM {table} WHERE {condition}")
+    for a in arks:
+        ark_obj = Ark(
+            ark=a["ark"],
+            target=a["_target"],
+            profile=a["_profile"],
+            status=a["_status"],
+            owner=a["_owner"],
+            ownergroup=a["_ownergroup"],
+            created=int(a["_created"]),
+            updated=int(a["_updated"]),
+            export=True if a["_export"] == "yes" else False,
+            dc_creator=a["dc.creator"],
+            dc_title=a["dc.title"],
+            dc_type=a["dc.type"],
+            dc_date=a["dc.date"],
+            dc_publisher=a["dc.publisher"],
+            erc_when=a["erc.when"],
+            erc_what=a["erc.what"],
+            erc_who=a["erc.who"],
+            replaceable=input_is_replaceable(a["dc.title"]),
+        )
+
+        session.add(ark_obj)
+
+    session.commit()
+    session.close()
+
+
+def input_is_replaceable(title):
+    """Determine of an ARK can be replaced with a new target.
+
+    Occassionally, ARKs have been created for children of a compound object.
+    On their own, these ARKs serve no useful purpose. Instead of leaving them,
+    we can mark them as replaceable so that we can update already-minted ARKs
+    with entirely new metadata and avoid wasting our ARKs.
+
+    Parameters
+    ----------
+    title : str
+        The title of the object in the ARK record. Child objects that don't
+        need their own ARKs can be identified by titles like 'Front', 'Back',
+        'Page 2', 'p. 7', and even ''.
+    
+    Returns
+    -------
+    bool
+    """
+    bad_title = re.compile(r"(^([Pp](age|\.) \d+|[Ff]ront|[Bb]ack)$|^$)")
+
+    return bool(bad_title.match(title))
+
+
+def url_is_in_db(resource):
+    session = Session()
+    # if session.query(Ark.target).filter_by(resource)
+
+
+def add_to_db(session, ark_obj):
+    session = Session()
+    pass
+
+
+def find_by_ark(ark):
+    pass
+
+
+def find_by_replaceable_arks():
+    session = Session()
+    results = session.query(Ark.replaceable).filter_by(True)
+    session.close()
+
+    return results
--- arkimedes/ezid.py
+++ arkimedes/ezid.py
@@ -7,49 +7,6 @@ import requests
 from arkimedes import CONCAT_STRING
 
 
-def build_anvl(
-    creator,
-    title,
-    dates,
-    target,
-    publisher="Iowa State University Library",
-    type_="Collection",
-    profile="dc",
-):
-    return cleandoc(
-        f"""erc.who:{creator}
-            erc.what:{title}
-            erc.when:{dates}
-            dc.creator:{creator}
-            dc.title:{title}
-            dc.publisher:{publisher}
-            dc.date:{dates}
-            dc.type:{type_}
-            _target:{target}
-            _profile:{profile}
-            """
-    )
-
-
-def upload_anvl(user_name, password, shoulder, anvl_text, output_file):
-    base_url = "https://ezid.cdlib.org/shoulder"
-    headers = {"Content-Type": "text/plain; charset=UTF-8"}
-
-    request_url = "/".join([base_url, shoulder])
-
-    r = requests.post(
-        request_url, headers=headers, data=anvl_text, auth=(user_name, password)
-    )
-
-    print(anvl_text)
-    print(r.text)
-
-    with open(output_file, "a", encoding="utf-8") as fh:
-        fh.write(anvl_text)
-        fh.write(r.text)
-        fh.write(CONCAT_STRING)
-
-
 def batch_download(username, password, format_="anvl", compression="zip", *args):
     """Batch download ARKs from EZID.
 
@@ -75,14 +32,14 @@ def batch_download(username, password, format_="anvl", compression="zip", *args)
     url = "https://ezid.cdlib.org/download_request"
     headers = {"Content-type": "application/x-www-form-urlencoded"}
     query = f"format={format_}&compression={compression}&{'&'.join(args)}"
-    
+
     r = requests.post(url, data=query, auth=(username, password), headers=headers)
 
     if not r.ok or not r.text.startswith("success: "):
-        print(f"{r.text}\n request failed")
+        print(f"Request failure!\n----------------\n\n{r.text}")
         sys.exit(1)
 
-    print("waiting..", end="")
+    print("Downloading ARK records from EZID..", end="")
 
     url = r.text[9:]
     file_name = url[url.rfind("/") + 1 :]
@@ -100,4 +57,75 @@ def batch_download(username, password, format_="anvl", compression="zip", *args)
     if r.status_code == 200:
         print(file_name)
     else:
-        print(f"download failed\nurl: {url}")
+        print(f"Download failed\nurl: {url}")
+
+
+def build_anvl(
+    creator,
+    title,
+    dates,
+    target,
+    publisher="Iowa State University Library",
+    type_="Collection",
+    profile="dc",
+):
+    return cleandoc(
+        f"""erc.who:{creator}
+            erc.what:{title}
+            erc.when:{dates}
+            dc.creator:{creator}
+            dc.title:{title}
+            dc.publisher:{publisher}
+            dc.date:{dates}
+            dc.type:{type_}
+            _target:{target}
+            _profile:{profile}
+            """
+    )
+
+
+def load_arks_from_ezid_anvl(anvl_file):
+    """Creates a generator that yields ARK records as dictionaries.
+
+    Parameters
+    ----------
+    anvl_file : str or pathlib.Path
+        File to load ARK records from.
+
+    Returns
+    -------
+    generator
+        Yields a dictionary where each key, value pair corresponds to an
+        ANVL record field.
+    """
+    ark_dict = {}
+    with open(anvl_file, "r", encoding="utf-8") as fh:
+        for line in fh:
+            line = line.strip()
+            if line == "":
+                yield ark_dict
+            elif line.startswith("::"):
+                ark_dict["ark"] = line[3:]
+            else:
+                k, v = line.split(": ", 1)
+                ark_dict[k] = v
+
+
+def upload_anvl(user_name, password, shoulder, anvl_text, output_file=None):
+    base_url = "https://ezid.cdlib.org/shoulder"
+    headers = {"Content-Type": "text/plain; charset=UTF-8"}
+
+    request_url = "/".join([base_url, shoulder])
+
+    r = requests.post(
+        request_url, headers=headers, data=anvl_text, auth=(user_name, password)
+    )
+
+    print(anvl_text)
+    print(r.text)
+
+    if output_file is not None:
+        with open(output_file, "a", encoding="utf-8") as fh:
+            fh.write(anvl_text)
+            fh.write(r.text)
+            fh.write(CONCAT_STRING)
